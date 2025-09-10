7de6184c7e99537b6daf7f8f983c91492144d0e8
tcarmo2023 <148493266+tcarmo2023@users.noreply.github.com>
2025-09-10 15:36:24 -0300

    Update build.sh

--- normaq-treinamentos/build.sh
+++ normaq-treinamentos/build.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-echo "Copiando arquivos estáticos..."
 cd backend
-mkdir -p target/classes/static
-cp -r src/main/resources/static/* target/classes/static/
 mvn clean package -DskipTests
-echo "Build concluído!"
